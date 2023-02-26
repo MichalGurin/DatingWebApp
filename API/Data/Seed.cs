8b5d7125e866ba79d66386ada568843e763c8461
MichalGurin <michal.gurin@gmail.com>
2023-02-26 19:04:30 +0100

    Docker and fly.io files. API now uses PostgreSql.

--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -8,6 +8,13 @@ namespace API.Data
 {
     public static class Seed
     {
+
+        public static async Task ClearConnectionsTable(DataContext context)
+        {
+            context.Connections.RemoveRange(context.Connections);
+            await context.SaveChangesAsync();
+        }
+        
         public static async Task SeedUsers(UserManager<AppUser> userManager, RoleManager<AppRole> roleManager)
         {
             if(await userManager.Users.AnyAsync()) return;
@@ -33,6 +40,8 @@ namespace API.Data
             foreach (AppUser user in users)
             {
                 user.UserName = user.UserName.ToLower();
+                user.Created = DateTime.SpecifyKind(user.Created, DateTimeKind.Utc);
+                user.LastActive = DateTime.SpecifyKind(user.LastActive, DateTimeKind.Utc);
                 await userManager.CreateAsync(user, "Pa$$w0rd");
                 await userManager.AddToRoleAsync(user, "Member");
             }
--- API/Program.cs
+++ API/Program.cs
@@ -15,6 +15,34 @@ builder.Services.AddControllers();
 builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddIdentityServices(builder.Configuration);
 
+var connString = "";
+if(builder.Environment.IsDevelopment())
+{
+      connString = builder.Configuration.GetConnectionString("DefaultConnection");
+}
+else
+{
+    // Use connection string provided at runtime by FlyIO.
+    var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+    // Parse connection URL to connection string for Npgsql
+    connUrl = connUrl.Replace("postgres://", string.Empty);
+    var pgUserPass = connUrl.Split("@")[0];
+    var pgHostPortDb = connUrl.Split("@")[1];
+    var pgHostPort = pgHostPortDb.Split("/")[0];
+    var pgDb = pgHostPortDb.Split("/")[1];
+    var pgUser = pgUserPass.Split(":")[0];
+    var pgPass = pgUserPass.Split(":")[1];
+    var pgHost = pgHostPort.Split(":")[0];
+    var pgPort = pgHostPort.Split(":")[1];
+
+    connString = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};";
+}
+builder.Services.AddDbContext<DataContext>(opt => 
+{
+      opt.UseNpgsql(connString);
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -49,7 +77,7 @@ try
 {
       var context = services.GetRequiredService<DataContext>();
       await context.Database.MigrateAsync();
-      await context.Database.ExecuteSqlRawAsync("DELETE FROM [Connections]"); //Clear all message connection during startup
+      await Seed.ClearConnectionsTable(context); //Clear all message connections during startup
       await Seed.SeedUsers(
             services.GetRequiredService<UserManager<AppUser>>(),
             services.GetRequiredService<RoleManager<AppRole>>());
